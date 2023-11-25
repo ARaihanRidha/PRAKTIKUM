5228d1c942f662082f6114f608f8d922f1205b68
ARaihanRidha <2210817110001@mhs.ulm.ac.id>
2023-11-25 12:24:49 +0800

    Praktikum5

---
+++ PRAKTIKUM05/module/src/Soal1/Cylinder.java
@@ -0,0 +1,16 @@
+package Soal1;
+public class Cylinder extends Shape{
+    private double radius;
+    private double height;
+    public Cylinder(double r, double h) {
+        super("Cylinder");
+        this.radius = r;
+        this.height = h;
+    }
+    public double area(){
+        return 3.14*radius*radius*height;
+    }
+    public String toString() {
+        return super.toString() + " (radius) " + radius + " and (height) " + height;
+    }
+}
